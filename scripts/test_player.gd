098db94f077379b115648f6a707af1b40efac82a
Diesel Thomas <151065987+dthomas1202@users.noreply.github.com>
2026-02-20 19:48:02 -0700

    Working map and background tile generator

--- scripts/test_player.gd
+++ scripts/test_player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
+@export var speed: float = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _physics_process(_delta: float) -> void:
-	pass
+	var input: float = Input.get_axis("player_left","player_right")
+	velocity = Vector2(input, 0) * speed
+	move_and_slide()
+	
+
+	
