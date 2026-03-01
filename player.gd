1e9753f02044068439f7d6c67e78d2783acffd22
rileymoore13 <tmoor995@mtroyal.ca>
2026-03-01 01:32:22 -0700

    created moss slug scene with sprite and script that causes the lil guy to track the player position with a frame delay. Added a hitbox, but will need to figure out how to make the hit box different based on the current sprite frame (maybe do the same for the player too). I have an idea of how to do this.

--- player.gd
+++ player.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var speed = 200
 @onready var screensize = get_viewport_rect().size
 
+var position_history = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -20,6 +22,9 @@ func _process(delta: float) -> void:
 		$Character.frame = 5
 	
 	position += input * speed * delta
-	
-	
 	position = position.clamp(Vector2(20,45), screensize - Vector2(20,45))
+	
+	position_history.append(global_position)
+	
+	if position_history.size()>300:
+		position_history.pop_front()
---
+++ slug.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@export var delay_frames := 18  # how many frames behind the player to follow
+@export var speed := 150
+
+var player
+
+func _ready():
+	player = get_node("../Player")  # adjust path as needed
+
+func _process(delta):
+	
+	
+	
+	
+	#var history = player.position_history
+	
+	
+
+	if player.position_history.size() > delay_frames:
+		var target_pos = player.position_history[player.position_history.size() - delay_frames]
+		var direction = (target_pos - global_position).normalized()
+
+		# Movement
+		global_position += direction * speed * delta
+
+		# Animation
+		if direction.x > 0.1:
+			$Character.frame = 5
+		elif direction.x < -0.1:
+			$Character.frame = 4
+		else:
+			$Character.frame = 6
