831b982992b2aee2ee056aefce91b8c25715e84f
rileymoore13 <tmoor995@mtroyal.ca>
2026-03-11 22:47:31 -0600

    Changed Player scene to use CharacterBody2D node as parent node, Area2D is now a child node. The biggest change as a result of this is that movement is now being handled by move_and_slide() in the _physics_process function.Frame management and input remain in the _process function. The _process function in the moss_slug script is now also _physics_process function to make sure slug following remains smooth. Also made variable types and function returns explicit to comply with project requirements. This involved making a Player class name for the Player node (the CharacterBody2D node).

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,35 +1,53 @@
-extends Area2D
+extends CharacterBody2D
+
+#Make Player a class name so moss slug script can init player variable as type Player
+class_name Player
 
 #Add player speed as a variable in inspector
-@export var speed = 200
+@export var speed: int = 200
 
-#
-@onready var screensize = get_viewport_rect().size
+# We aren't using this variable right now but I suspect it will come in handy...maybe not
+#@onready var screensize: Vector2 = get_viewport_rect().size
+
+#create character variable for the Node as type Sprite2D
+@onready var character: Sprite2D = $Character
 
 #init position history list for moss slug script
-var position_history = []
+var position_history: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+# How we are handling movement WITH Character2DBody (using move_and_slide)
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+	
+	#Tracks player position for use in moss_slug script
+	position_history.append(global_position.x)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var input = Input.get_axis("player_left", "player_right")
+	var input: float = Input.get_axis("player_left", "player_right")
 
 #set sprite frame to left slide for left input, right frame for right input, straight frame for no input
 	if input > 0:
-		$Character.frame = 7
+		character.frame = 7
 	elif input < 0:
-		$Character.frame = 6
+		character.frame = 6
 	else:
-		$Character.frame = 5
+		character.frame = 5
 	
-	#manage horizontal movement with input, speed, delta
-	position += Vector2(input, 0) * speed * delta
-
-	#append players position to the position history list
-	position_history.append(global_position.x)
+	#Update velocity
+	velocity.x = input * speed
+	
+	
+# How we were handling movement before using CharacterBody2D
+	##apply velocity
+	#position += velocity * delta
+	#
+	##append players position to the position history list
+	#position_history.append(global_position.x)
 	
 	
