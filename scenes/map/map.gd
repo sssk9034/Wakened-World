c2e4462acd23202a3dcbdc39bd9bb4234b262644
Diesel Thomas <151065987+dthomas1202@users.noreply.github.com>
2026-03-16 02:02:18 -0600

    Implemented movement and loading/unloading of map tiles. Created MapTile scene and two inherited scenes from it.

--- scenes/map/map.gd
+++ scenes/map/map.gd
@@ -1,3 +1,4 @@
+class_name Map
 extends Node2D
 ## Handles smooth map movement.
 ##
@@ -11,23 +12,47 @@ extends Node2D
 ## set_velocity(new_velocity: float) -> void
 ##		Instantly changes the maps velocity, (aka, infinite acceleration)
 
+const TILES_PER_MAP_LAYER: int = 10
+
+@export_group("Generation")
+@export var map_seed: String = ""
+@export var map_length: int = 0 # Set to 0 to generate based on map_seed
+
+@export_group("Movement")
 @export var acceleration: float = 100.0 # pixels/sec^2
 @export var deceleration: float = 200.0 # pixels/sec^2
 @export var velocity: float = 100.0 # pixels/sec
 
 var _current_velocity: float = 0.0 # pixels/sec
 
+@onready var _leading_map_layer: MapLayer = $MapLayer1
+@onready var _following_map_layer: MapLayer = $MapLayer2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Trigger initial build
 	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+# Called every physics frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
 	_update_velocity(delta)
-	_update_map_position(delta)
 	
+	if _check_for_layer_swap():
+		print("[Map]: Swapping MapLayers")
+		_leading_map_layer.clear()
+		
+		var map_layer: MapLayer = _leading_map_layer
+		_leading_map_layer = _following_map_layer
+		_following_map_layer = map_layer
+		
+		_update_map_layer_position(delta)
+		
+		# TODO trigger build of following
+		
+	else:
+		_update_map_layer_position(delta)
+		
 
 ## Smoothly changes velocity to new_velocity by acceleration values.
 func change_velocity(new_velocity: float) -> void:
@@ -56,6 +81,24 @@ func _update_velocity(delta: float) -> void:
 	
 
 ## Moves map by current_velocity
-func _update_map_position(delta: float) -> void:
-	move_local_y(-_current_velocity * delta, false)
+func _update_map_layer_position(delta: float) -> void:
+	_leading_map_layer.move_local_y(-_current_velocity * delta, false)
+	_following_map_layer.position = _leading_map_layer.get_end_position()
+	
+
+# Check if _leading_map_layer is fully off screen.
+# This happens when _following_map_layer.global_position.y is above the camera.
+func _check_for_layer_swap() -> bool:
+	#var pos: float = get_viewport().get_camera_2d().get_screen_center_position().y
+	#var offset: float = (get_viewport_rect().size.y / get_viewport().get_camera_2d().zoom.y) / 2
+	
+	# Optimizations based on assumptions
+	# Since the camera should always be at y = 0
+	var pos: float = 0.0
+	# Camera zoom should never be less than 0.5
+	var offset: float = get_viewport_rect().size.y
+
+	return _following_map_layer.global_position.y < (pos - offset)
+	
+	
 	
---
+++ scenes/map/map_layer.gd
@@ -0,0 +1,35 @@
+class_name MapLayer
+extends Node2D
+
+var _next_tile_position: Vector2 = Vector2(165, 1074)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+# Appends the given tile to the layer.
+func add_tile(tile: MapTile) -> void:
+	pass
+	
+
+# Clears all tiles from the layer.
+func clear() -> void:
+	pass
+	
+	
+# Returns the position of the end of the map.
+# Position is relative to the parent node (like .position).
+func get_end_position() -> Vector2:
+	return position + _next_tile_position
+	
+	
+# Returns the position of the end of the map.
+# Position is relative to this node.
+func get_relative_end_position() -> Vector2:
+	return _next_tile_position
