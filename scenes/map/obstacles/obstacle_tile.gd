1684394508046e35cd84cb2cb39a4fc526a705be
Diesel Thomas <151065987+dthomas1202@users.noreply.github.com>
2026-03-17 01:05:56 -0600

    Initial version of obstacle detection

--- scenes/map/obstacles/obstacle_tile.gd
+++ scenes/map/obstacles/obstacle_tile.gd
@@ -12,6 +12,8 @@ const POSITION_RIGHT: int = 4
 @export var allow_double_obstacle: bool = true
 @export var probability: float = 1.0
 
+var _velocity_modifier: Map.VelocityModifier = StopVelocityModifier.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -20,3 +22,21 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+	
+	
+# Called when the player enters the obstacle bounds.
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	Map.singleton.add_current_velocity_modifier(_velocity_modifier)
+
+
+# Called when the player exits the obstacle bounds.
+func _on_area_2d_body_exited(_body: Node2D) -> void:
+	Map.singleton.remove_current_velocity_modifier(_velocity_modifier)
+
+
+class StopVelocityModifier extends Map.VelocityModifier:
+	func _init() -> void:
+		priority = 100
+	
+	func mod_velocity(_old_velocity: float, _original_velocity: float) -> float:
+		return 0.0
