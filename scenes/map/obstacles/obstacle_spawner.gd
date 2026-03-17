f6f2c7d0ec0e4c3a0dc1c7d7076f3eff65a249a6
Diesel Thomas <151065987+dthomas1202@users.noreply.github.com>
2026-03-17 17:10:14 -0600

    Work on obstacle spawner/builder

--- scenes/map/obstacles/obstacle_spawner.gd
+++ scenes/map/obstacles/obstacle_spawner.gd
@@ -1,12 +1,43 @@
+@tool
 class_name ObstacleSpawner
 extends Node2D
+##
 
+@export_group("Generation")
+@export var obstacle_builder: ObstacleBuilder
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export_group("Positioning")
+@export_range(-100, 100, 0.01, "suffix:px") var left_position: float = -60.0:
+	set(value):
+		left_position = value
+		queue_redraw()
+@export_range(-100, 100, 0.01, "suffix:px") var center_position: float = 0.0:
+	set(value):
+		center_position = value
+		queue_redraw()
+@export_range(-100, 100, 0.01, "suffix:px") var right_position: float = 60.0:
+	set(value):
+		right_position = value
+		queue_redraw()
 
+func _init() -> void:
+	# Build obstacles
+	var struct: ObstacleStructure = obstacle_builder.get_next_obstacle_set()
+	
+	_place_obstacle(left_position, struct.left)
+	_place_obstacle(center_position, struct.center)
+	_place_obstacle(right_position, struct.right)
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _draw() -> void:
+	if Engine.is_editor_hint():
+		draw_circle(Vector2(left_position, 0), 3.0, Color(1.0, 0.0, 0.0))
+		draw_circle(Vector2(center_position, 0), 3.0, Color(0.0, 0.0, 1.0))
+		draw_circle(Vector2(right_position, 0), 3.0, Color(0.0, 1.0, 0.0))
+	
+	
+func _place_obstacle(offset: float, obstacle: ObstacleTile) -> void:
+	obstacle.position.x = offset
+	add_child(obstacle)
+	
+	
---
+++ scenes/map/obstacles/obstacle_structure.gd
@@ -0,0 +1,7 @@
+class_name ObstacleStructure
+extends Object
+## Structure for a list of obstacles
+
+var left: ObstacleTile
+var center: ObstacleTile
+var right: ObstacleTile
