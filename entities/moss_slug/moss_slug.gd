e15bb8f0771f070bebe9bc73632bda9cb33bc585
unknown <ezzidean1@gmail.com>
2026-03-23 18:02:42 -0600

    monster roar added

--- entities/moss_slug/moss_slug.gd
+++ entities/moss_slug/moss_slug.gd
@@ -2,23 +2,28 @@ extends Area2D
 
 @onready var character: AnimatedSprite2D = $Character
 @onready var drag_path: AnimatedSprite2D = $DragPath
-
+@onready var roar: AudioStreamPlayer2D = $Roar
 
 @export var delay_frames : int = 25  # how many frames behind the player to follow
 @export var speed: int = 150 # slug speed
 
 @export var player: Player
 
-func _ready()-> void:
+# Track if the monster has moved and already roared
+var has_roared_this_move: bool = false
+
+func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	
 	if player.position_history.size() > delay_frames:
 		var target_x: float = player.position_history[0]
 		player.position_history.remove_at(0)
 		
 		var distance: float = target_x - global_position.x
+
+		
+		check_and_play_roar(distance)
 
 		if abs(distance) > 1.0:
 			# Movement
@@ -31,7 +36,13 @@ func _physics_process(delta: float) -> void:
 			elif distance < 0:
 				character.animation = "left"
 				drag_path.animation = "left"
-
 		else:
 			character.animation = "straight"
 			drag_path.animation = "straight"
+
+#makes sure to play the roar at the start of the game
+func check_and_play_roar(distance: float) -> void:
+	if abs(distance) > 1.0:
+			has_roared_this_move = true
+			roar.stop()
+			roar.play()
