e95a13958cf836b000ee50bf3e3c33676e89ec9d
Diesel Thomas <151065987+dthomas1202@users.noreply.github.com>
2026-03-28 00:44:54 -0600

    Improved death screen retry button, and added button fade in

--- ui/death/death_scene.gd
+++ ui/death/death_scene.gd
@@ -1,23 +1,35 @@
 extends CanvasLayer
 
 var _main_scene: PackedScene = load("res://scenes/main/main_game.tscn")
-@onready var _fade_animation: AnimationPlayer = $FadeAnimation
 
+@onready var _fade_animation: AnimationPlayer = $FadeAnimation
+@onready var _retry_button: Button = $Control/VBoxContainer/AspectRatioContainer/MarginContainer/Button
 
 func _ready() -> void:
+	_disable_retry_button()
 	await _fade_animation_func()
 	
+func _disable_retry_button() -> void:
+	_retry_button.disabled = true
+	_retry_button.modulate = Color(1, 1, 1, 0)
+	
+func _fade_retry_button() -> void:
+	_retry_button.disabled = false
+	get_tree().create_tween() \
+		.tween_property(_retry_button, "modulate", Color.WHITE, 0.5)
 
 func _fade_animation_func() -> void:
 	_fade_animation.play("FadeAnimation")
 	await _fade_animation.animation_finished
+	_fade_retry_button()
+	
 
 func _fade_animation_reverse() -> void:
 	_fade_animation.play_backwards("FadeAnimation")
 	await _fade_animation.animation_finished
 
 
-func _on_texture_button_pressed() -> void:
+func _on_button_pressed() -> void:
 	get_tree().root.add_child(_main_scene.instantiate())
 	await _fade_animation_reverse()
 	queue_free()
