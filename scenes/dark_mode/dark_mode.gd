c33863c68dc2454e890d8b650af3c0dcc1b672c4
Diesel Thomas <151065987+dthomas1202@users.noreply.github.com>
2026-04-05 01:31:30 -0600

    Fixed tabs in dark_mode.gd

--- scenes/dark_mode/dark_mode.gd
+++ scenes/dark_mode/dark_mode.gd
@@ -9,27 +9,27 @@ extends Node2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-    if not enabled:
-        visible = false
-        process_mode = Node.PROCESS_MODE_DISABLED
-    
-    # Update position of player lights
-    _flashlight.global_position = player.global_position
+	if not enabled:
+		visible = false
+		process_mode = Node.PROCESS_MODE_DISABLED
+	
+	# Update position of player lights
+	_flashlight.global_position = player.global_position
 
 
 func switch_to_player_light() -> void:
-    if not enabled:
-        return
-    
-    var tween: Tween = get_tree().create_tween()
-    tween.tween_property(_canvas_modulate, "color", Color(0.0, 0.0, 0.0, 1.0), 2.5)
-    tween.tween_property(_flashlight, "enabled", true, 0)
-    tween.tween_callback(_flashlight.flicker)
-        
+	if not enabled:
+		return
+	
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(_canvas_modulate, "color", Color(0.0, 0.0, 0.0, 1.0), 2.5)
+	tween.tween_property(_flashlight, "enabled", true, 0)
+	tween.tween_callback(_flashlight.flicker)
+		
 func switch_to_game_light() -> void:
-    if not enabled:
-        return
-    
-    var tween: Tween = get_tree().create_tween()
-    tween.tween_property(_canvas_modulate, "color", Color(0.282, 0.282, 0.282, 1.0), 2.5)
-    tween.tween_property(_flashlight, "enabled", false, 0)
+	if not enabled:
+		return
+	
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(_canvas_modulate, "color", Color(0.282, 0.282, 0.282, 1.0), 2.5)
+	tween.tween_property(_flashlight, "enabled", false, 0)
